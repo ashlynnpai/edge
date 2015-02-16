6b7c52dddadffd56d2194c970f234c6a4410b005
ashlynnpai <ashlynnpai@gmail.com>
2015-02-16 17:58:48 +0000

    courseitem views

--- app/controllers/courseitems_controller.rb
+++ app/controllers/courseitems_controller.rb
@@ -1,6 +1,10 @@
 class CourseitemsController < ApplicationController
   before_filter :require_user
   
+  def index
+    @courseitems = current_user.courseitems
+  end
+  
   def create
     @course = Course.find_by slug: params[:course_id]
     Courseitem.create(course: @course, user: current_user, position: last_position) unless current_user.courseitems.map(&:course).include?(@course)
