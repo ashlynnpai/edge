920dcc1954d289b74f4dd5eee4db3e09d10fa678
Ashlynn Pai <ashlynnpai@gmail.com>
2015-08-09 21:39:53 +0000

    github url

--- app/models/github.rb
+++ app/models/github.rb
@@ -1,6 +1,12 @@
 class Github
   include HTTParty
   
+  def self.response
+    user = @badgeinput.github_name
+    api_url = "https://api.github.com/users/#{user}/events/public"
+    HTTParty.get(api_url)
+  end
+  
 end
 
 
